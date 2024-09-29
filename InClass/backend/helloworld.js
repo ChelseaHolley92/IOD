15e96fcd027c6476ac7beb38559387bf4d6c21b5
Your Username <ChelseaHolley23@gmail.com>
2024-09-29 15:10:09 -0400

    commit

--- InClass/backend/helloworld.js
+++ InClass/backend/helloworld.js
@@ -1,1 +1,28 @@
-console.log('Hello World')
+//this is the structure of a function
+//function ThisIsTheName(Signature){
+//  Body
+// }
+
+function sayHello() {
+console.log('Hello Chelsea')
+}
+
+sayHello
+function sum(a, b) {
+ }  
+console.log(sum(5, 2))
+
+function sum(1, 2) {
+    console.log(1+2)
+}
+sum(1, 2) 
+   
+const Sentiment =require('sentiment')
+const Sentiment =new Sentiment();
+const result =sentiment.analyze('Cats are stupid');
+console.log(result);    //Score: -2, Comparative: -0.666
+
+
+
+
+  
