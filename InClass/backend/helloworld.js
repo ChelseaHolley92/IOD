0b3d88622049a7930a90ae272f0ccb7b8d6bb713
Your Username <ChelseaHolley23@gmail.com>
2024-09-29 17:44:09 -0400

    commit

--- InClass/backend/helloworld.js
+++ InClass/backend/helloworld.js
@@ -1,1 +1,16 @@
-console.log('Hello World')
+// this is the structure of a function
+// function ThisIsTheName(Signature) {
+//  Body
+// }
+
+function sayHello() {
+    console.log('Hello Chelsea')
+}
+
+sayHello();
+
+ function sum(a,b) {
+    sum(5,1)
+    console.log(a+b)
+ }
+sum(5, 2);
