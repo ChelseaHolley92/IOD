ea6a609ae1b8925c101cb318e9be7d94e3b632f1
Your Username <ChelseaHolley23@gmail.com>
2024-09-30 21:17:51 -0400

    commit

---
+++ InClass/backend/book.js
@@ -0,0 +1,42 @@
+//Try creating a json object variable for a book.The book should have a title, decription, author, and number of pages
+
+let book= {
+title: "The Hitchhiker's Guide to the Galaxy"
+description: "The adventures of the only man to survive the destruction"
+author: "Douglas Adams"
+nu,Pages: 216
+}
+
+//try printing these object property values in your console individually
+
+console.log(book.title)
+console.log(book,description)
+console.log(book.author)
+console.log(book.numpages)
+console.log(book)
+
+//Update the description of the book
+book.description="Seconds before the earth is demolished..."
+console.log(book.decription)
+
+//Createan array of 5 books objects
+
+let books= [
+
+    title: "The Night Circus",
+    description:"Magical competition between two young illusionists",
+    author:"Erin Morgenstern",
+    numPages: 387
+} ,
+{
+    title: "The Martian"
+    description: "an Astouant becomes stranded on mars and needs to find a way to survive"
+    author: "Andy Meir"
+    numPages:"369"
+},
+{
+    title: "Where the crawdads sing"
+    description: "A mystery set in the narshes at North Carolina"
+    author: "Delia Owens"
+    numPages: "368"
+
---
+++ InClass/backend/json.js
@@ -0,0 +1,21 @@
+//objects in javascript contain keys (or properties) with corresponding values
+
+const user={
+    "first_name" :"Sammy" ,
+    "last_name" :"shark" ,
+    "age" : 25
+    "followers" :987
+}
+
+console.log(user.first_name)
+console.log(user.age)
+
+console.log({"last_name"})}
+
+user.follwers=998
+
+console.log(user.followers)
+
+user.location='Pacific Ocean
+
+console.log(user.location)
